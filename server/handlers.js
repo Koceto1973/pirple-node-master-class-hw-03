aba308893a6832077a7a06cd143e0c6f925ebf55
TeamK <koceto1973@yahoo.ca>
2019-02-22 11:59:49 +0200

    favicons in

--- server/handlers.js
+++ server/handlers.js
@@ -41,24 +41,6 @@ handlers.template = function(data,callback){ // callback(200,str,'html')
   }
 };
 
-// Favicon
-handlers.favicon = function(data,callback){ // callback(200,data,'favicon')
-  // Reject any request that isn't a GET
-  if(data.method == 'get'){
-    // Read in the favicon's data
-    helpers.getStaticAsset('favicon.ico',function(err,data){
-      if(!err && data){
-        // Callback the data
-        callback(200,data,'favicon');
-      } else {
-        callback(500);
-      }
-    });
-  } else {
-    callback(405);
-  }
-};
-
 // Static assets
 handlers.static = function(data,callback){ // callback(200,data,contentType);
   // Reject any request that isn't a GET
